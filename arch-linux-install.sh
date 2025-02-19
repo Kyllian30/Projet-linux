8f9e6decdbf87c8c96ba3c8f8a01fd93cbe84a4d
kyllian <kyllian.caiola06@gmail.com>
2025-02-19 12:15:48 +0100

    Ajout du script avec LF 2

--- arch-linux-install.sh
+++ arch-linux-install.sh
@@ -9,9 +9,8 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Vérification que l'OS est Ubuntu ou Debian
-if ! grep -qi 'ubuntu\|debian' /etc/os-release; then
-    printf "Erreur : Ce script est conçu pour être lancé depuis un système Ubuntu ou Debian (installé).\n"
+if ! grep -qi 'arch' /etc/os-release; then
+    printf "Erreur : Ce script est conçu pour être lancé sur Arch Linux.\n"
     exit 1
 fi
 
